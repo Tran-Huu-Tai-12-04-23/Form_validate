56752a307ba29ade9c921fb049dc8da56870a258
dev <huutt201@gmail.com>
2022-11-05 15:15:09 +0700

    fix

--- validation.js
+++ validation.js
@@ -1,23 +1,35 @@
 function validation(form) {
-  const formRules = {};
+    const formRules = {};
 
-  rules = {
-    required: (nameInput, val) => {
-      return val ? `Please, enter ${nameInput}` : "Correct!!!";
-    },
-    pass: (nameInput, val, val2) => {
-      return 1;
-    },
-    min: (nameInput, val, valmin) => {
-      return val.length < valmin
-        ? `You must enter a least ${valmin} characters!!!`
-        : "Correct!!!";
-    },
-  };
-  var formValidation = document.querySelector(form);
-  console.log(formValidation);
-  if (formValidation) {
-    var input = formValidation.querySelectorALL("input");
-    console.log(input);
-  }
+    rules = {
+        required: (nameInput, val) => {
+            return val ? `Please, enter ${nameInput}` : "Correct!!!";
+        },
+        pass: (nameInput, val, val2) => {
+            return 1;
+        },
+        min: (nameInput, val, valmin) => {
+            return val.length < valmin ?
+                `You must enter a least ${valmin} characters!!!` :
+                "Correct!!!";
+        },
+    };
+    var formValidation = $(form);
+    if (formValidation) {
+        var input = formValidation.children().children('.input__form__login');
+        input.each(function() {
+            const rules = $(this).attr('rules').split('|')
+            for (var it of rules)
+                if (it.toString().includes(':')) {
+                    var rulesDetail = it.split(':');
+                }
+            if (Array.isArray(formRules)) {
+
+            } else {
+                // formRules[]
+                console.log(it);
+            }
+        })
+
+    }
 }
